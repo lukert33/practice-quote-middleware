cc1e90291cad997a6e10286a9fe32c1efaf91b85
Luke Thomas <lukert33@gmail.com>
2015-02-04 14:10:23 -0500

    Quoter; build instance from file_path.

--- app/quoter.rb
+++ app/quoter.rb
@@ -1,3 +1,23 @@
 class Quoter
 
+  attr_reader :quotes
+
+  def initialize(file_path)
+    quotes = array_of_lines(file_path)
+    @quotes = no_linebreaks(quotes)
+  end
+
+
+  private
+  def no_linebreaks(string_array)
+    string_array.map{|str| str.gsub(/\n+/, "")}
+  end
+
+  def array_of_lines(file_path)
+    output = []
+    file = File.open(APP_ROOT + file_path, "r")
+    IO.readlines(file).each{|line| output << line }
+    output
+  end
+
 end
