bc88bc75a72e9b3d73b77c725c655a1ef8bdfead
Luke Thomas <lukert33@gmail.com>
2015-02-05 18:37:08 -0500

    Build different Quoters for rackup.

--- environment.rb
+++ environment.rb
@@ -3,5 +3,7 @@ require "pathname"
 APP_ROOT = Pathname.new(File.expand_path('../', __FILE__))
 
 Dir[APP_ROOT.join('app', '*.rb')].each { |file| load file }
+solar_system = Dir[APP_ROOT.join('spec', 'fixtures', 'planets', '*.txt')].sample
 
-APP_QUOTE = Quoter.new("spec/fixtures/rickygervais.txt").random_quote
+PLANET_TITLE = Quoter.new(solar_system).random_quote
+RICKY_QUOTE = Quoter.new("spec/fixtures/rickygervais.txt").random_quote
